519f89e5c1596a40be6596ef5d72403d978d0717
Yaccouv <mungoshiyacc4@gmail.com>
2025-05-08 16:12:54 +0200

    chh2

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ import Link from "next/link"
 import Image from "next/image"
 import { ArrowRight, Leaf, MapPin, Phone, Mail } from "lucide-react"
 
-//import { Button } from "@/components/ui/button"
-//import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
